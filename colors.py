6c4e140e28330eb26bb43c9d146fffa26dfa5aaa
harsha823 <harshaharshavardhan075@gmail.com>
2026-06-18 12:34:29 +0530

    color import

---
+++ colors.py
@@ -0,0 +1,9 @@
+# ANSI colors — imported everywhere, defined once
+GREEN   = "\033[92m"
+RED     = "\033[91m"
+YELLOW  = "\033[93m"
+CYAN    = "\033[96m"
+BLUE    = "\033[94m"
+MAGENTA = "\033[95m"
+BOLD    = "\033[1m"
+RESET   = "\033[0m"
